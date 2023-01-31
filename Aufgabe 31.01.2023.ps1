5d0c64a0cbdb3a7ed15c69efa45805b40ce59ae2
Thyral <sylixscott@gmail.com>
2023-01-31 10:37:10 +0100

    Aufgabe fertig

--- Aufgabe 31.01.2023.ps1
+++ Aufgabe 31.01.2023.ps1
@@ -1,6 +1,6 @@
-Get-Volume
+Get-Volume | Select-Object DriveLetter, FileSystemLabel, @{
+    #Füge eine neue Spalte hinzu, die den freien Speicherplatz angibt
+    Name="Freier Speicherplatz"
 
-$Speicherplatz = $(get-volume .driveletter c). SizeRemaining
-Write-Host $Speicherplatz
-
-#Test 
+    Expression={[math]::Round($_.SizeRemaining / 1GB, 2)}
+}
