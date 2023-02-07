901287e2dfa942b67c06e425cc74e0e97e5b4edc
Thyral <sylixscott@gmail.com>
2023-02-07 08:48:23 +0100

    Aufgabe bearbeitet

--- Aufgabe 07.02.2023.ps1
+++ Aufgabe 07.02.2023.ps1
@@ -1,1 +1,9 @@
-Stop-Process -Name powershell -WhatIf
+# Aufgabe zur Deklaration von Variablen mit jeweiligen Datentypen
+
+[int] $id = 1
+[long] $ticketnummer = 963927
+[float] $geldbetrag = 3.1419
+[double] $kontostand = 19.9383
+[string] $name = "TJ"
+[bool] $wert = "false"
+
