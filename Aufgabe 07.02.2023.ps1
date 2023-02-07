37a0052c8f37ec20136717664d65587992c7514b
Thyral <sylixscott@gmail.com>
2023-02-07 09:33:50 +0100

    Taschenrechner gemacht

--- Aufgabe 07.02.2023.ps1
+++ Aufgabe 07.02.2023.ps1
@@ -1,9 +1,52 @@
 # Aufgabe zur Deklaration von Variablen mit jeweiligen Datentypen
 
-[int] $id = 1
-[long] $ticketnummer = 963927
-[float] $geldbetrag = 3.1419
-[double] $kontostand = 19.9383
-[string] $name = "TJ"
-[bool] $wert = "false"
+#[int] $id = 1
+#[long] $ticketnummer = 963927
+#[float] $geldbetrag = 3.1419
+#[double] $kontostand = 19.9383
+#[string] $name = "TJ"
+#[bool] $wert = "false"
+
+
+# Aufgabe 2
+function abfrage() {
+    [int] $zahl1 = Read-Host("Bitte geben Sie die 1. Zahl ein")
+    [int] $zahl2 = Read-Host("Bitte geben Sie die 2. Zahl ein")
+   [int] $functionquest = Read-Host("Bitte geben Sie die Zahl 1 - 4 ein (1 = Addition; 2 = Subtraktion; 3 = Multiplikation; 4 = Divition)")
+
+    if($functionquest -eq 1) {
+        addition
+    } elseif ($functionquest -eq 2) {
+        subtraktion
+        <# Action when this condition is true #>
+    } elseif ($functionquest -eq 3 ) {
+        multiplikation
+    } else {
+        divition
+    }
+}
+
+function addition() {
+    [int] $ergebnis = $zahl1 + $zahl2
+    Write-Host($ergebnis)
+}
+   
+function subtraktion() {
+    $ergebnis = $zahl1 - $zahl2
+    Write-Host($ergebnis)
+}
+       
+function multiplikation() {
+    $ergebnis = $zahl1 * $zahl2
+    Write-Host($ergebnis)
+}
+       
+
+function divition() {
+    $ergebnis = $zahl1 / $zahl2
+    Write-Host($ergebnis)
+}
+       
+
+abfrage
 
